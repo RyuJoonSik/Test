6a51395abb398f90e63415a0903342027d096b54
RyuJoonSik <tldtksahtxja@naver.com>
2020-10-04 19:44:06 +0900

    Vertical Progressbar

---
+++ MyTest5/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace MyTest5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStartThread = new System.Windows.Forms.Button();
+            this.btnStopThread = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.verticalProgressBar1 = new MyTest5.verticalProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnStartThread
+            // 
+            this.btnStartThread.Location = new System.Drawing.Point(0, 0);
+            this.btnStartThread.Name = "btnStartThread";
+            this.btnStartThread.Size = new System.Drawing.Size(75, 23);
+            this.btnStartThread.TabIndex = 0;
+            this.btnStartThread.Text = "button1";
+            this.btnStartThread.UseVisualStyleBackColor = true;
+            this.btnStartThread.Click += new System.EventHandler(this.btnStartThread_Click);
+            // 
+            // btnStopThread
+            // 
+            this.btnStopThread.Location = new System.Drawing.Point(0, 55);
+            this.btnStopThread.Name = "btnStopThread";
+            this.btnStopThread.Size = new System.Drawing.Size(75, 23);
+            this.btnStopThread.TabIndex = 1;
+            this.btnStopThread.Text = "button2";
+            this.btnStopThread.UseVisualStyleBackColor = true;
+            this.btnStopThread.Click += new System.EventHandler(this.btnStopThread_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(260, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(123, 0);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(260, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // Timer
+            // 
+            this.Timer.Enabled = true;
+            // 
+            // verticalProgressBar1
+            // 
+            this.verticalProgressBar1.Location = new System.Drawing.Point(147, 55);
+            this.verticalProgressBar1.Name = "verticalProgressBar1";
+            this.verticalProgressBar1.Size = new System.Drawing.Size(40, 137);
+            this.verticalProgressBar1.Step = 1;
+            this.verticalProgressBar1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.verticalProgressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStopThread);
+            this.Controls.Add(this.btnStartThread);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartThread;
+        private System.Windows.Forms.Button btnStopThread;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer Timer;
+        private verticalProgressBar verticalProgressBar1;
+    }
+}
+
---
+++ MyTest5/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Windows.Forms.VisualStyles;
+
+namespace MyTest5
+{
+    public partial class Form1 : Form
+    {
+        private bool sw;
+        private Thread randTh, progTh;
+        private int x, y;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Thread로 수행할 작업
+       
+        private void Count()
+        {
+            while (sw)
+            {
+                // 1초 간격으로 난수 생성
+                Random r = new Random();
+                x = r.Next(1, 100);
+                y = r.Next(1, 100);
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void Prog()
+        {
+            while (sw)
+            {
+                // 만들어진 난수값 대입
+                progressBar1.Value = x;
+                label1.Text = progressBar1.Value.ToString();
+                verticalProgressBar1.Value = y;
+                label2.Text = verticalProgressBar1.Value.ToString();
+            }
+        }
+
+        private void btnStartThread_Click(object sender, EventArgs e)
+        {
+            randTh = new Thread(Count); // 난수생성 스레드
+            progTh = new Thread(Prog); // 난수값 대입 스레드
+            sw = true; // sw가 true 조건동안만 작동
+            randTh.Start();
+            progTh.Start();
+        }
+
+        private void btnStopThread_Click_1(object sender, EventArgs e)
+        {
+            // false시 스레드 정지
+            sw = false;
+        }
+    }
+}
